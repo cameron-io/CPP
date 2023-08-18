6a69c9a23288503e0e20fec3876738143e319862
Cameron Gallichan <camerongallichan@gmail.com>
2023-08-18 20:21:39 +0100

    refactored dep injection logic, docs

--- lib/oop/dependency_injection.hpp
+++ lib/oop/dependency_injection.hpp
@@ -22,16 +22,7 @@ public:
 class Car {
     GasolineSource *gasolineService = nullptr;
 public:
-    // The dependency for a source of gasoline is passed in
-    // through constructor injection
-    // as opposed to hard-coded into the class definition.
-    Car(GasolineSource *service)
-    : gasolineService(service) {
-        // If the dependency was not defined, throw an exception.
-        if(gasolineService == nullptr){
-            throw std::invalid_argument("service must not be null");
-        }
-    }
+    Car(GasolineSource *service);
     void getGasoline();
 };
 
