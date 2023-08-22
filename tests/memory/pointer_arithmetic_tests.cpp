218c1ad052d4827c1a648a010ede47888f40f116
Cameron Gallichan <camerongallichan@gmail.com>
2023-08-22 23:32:41 +0100

    improved pointer arithmetic testing

--- tests/memory/pointer_arithmetic_tests.cpp
+++ tests/memory/pointer_arithmetic_tests.cpp
@@ -1,40 +1,55 @@
 #include <memory>
 #include <iostream>
 
-void CountMemory(int* arr, int size)
+int CountMemory(int* arr, int size)
 {
-    // Replace initial zero-out values with memory counter
-    for (unsigned int i = 0; i < size; i++)
-    {
-        arr[i] = i * sizeof(int);
-    }
+    return size * sizeof(int);
 }
 
-void PrintArray(int* arr, int size)
+int* PrintArray(int* arr, int byte_count)
 {
-    // store array address in pointer
-    int* ptr = arr;
-    for (int i = 0; i < size; i++) {
-        std::cout << "Index: " << i << "\n";
-        std::cout << "-> *ptr: " << *ptr << "\n";
-        std::cout << "-> ptr: " << ptr << "\n";
-        std::cout << std::endl;
+    /*
+        store array address in pointer using type punning
+        this allows the specification of the pointer to locate
+        the single byte denoting the start of the array.
 
-        // point to the next location
-        ptr++;
+        the offset will be based on this value
+    */
+    char* ptr = (char*) arr;
+
+    int offset = 0;
+
+    while (offset < byte_count) {
+        /*
+            cast to integer to correctly interpret memory allocation 
+        */
+        int* address = (int *)(ptr + offset);
+
+        std::cout << "Offset (bytes): " << offset << "\n";
+        std::cout << "-> value: " << *address << "\n";
+        std::cout << "-> address: " << address << std::endl;
+
+        offset += sizeof(int);
     }
+
+    return arr;
 }
 
 int main ()
 {
     int size = 10;
-    int* arr = (int*) malloc(size * sizeof(int));
+    int* arr = (int*) malloc(size * sizeof(int));;
 
-    CountMemory(arr, size);
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = (i+1) * 5;
+    }
 
-    PrintArray(arr, size);
+    int byte_count = CountMemory(arr, size);
 
-    delete arr;
+    PrintArray(arr, byte_count);
+
+    free(arr);
 
     return 0;
 }
